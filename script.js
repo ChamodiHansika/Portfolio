68da850d2c0b43d17bee4618ee4ff7074c63362d
Chamodi Hansika <122684469+ChamodiHansika@users.noreply.github.com>
2024-06-25 08:58:12 +0530

    Add the Poartfolio in Fisrt commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example of a simple JavaScript function
+function showAlert() {
+    alert("Welcome to my portfolio website!");
+}
+
+// Call the function when the page loads
+window.onload = showAlert;
